68211b2f37bb13216a9b2e54b1843a4512bd2eff
Isidro J. Leonyork <ijleon@hotmail.com>
2023-10-26 15:40:40 -0400

    SignUp functionality is complete on client and server side.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 
 export const SignUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
   const saltRounds = 10;
 
   console.log(req.body);
 
-  const { username, email, password } = req.body;
   try {
     var salt = bcryptjs.genSaltSync(saltRounds);
     var hashPassword = bcryptjs.hashSync(password, salt);
@@ -23,4 +25,30 @@ export const SignUp = async (req, res, next) => {
   }
 }
 
-export default SignUp;
+export const SignIn = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  console.log(req.body);
+
+  try {
+    /* check email */
+    const validUser = await User.findOne({ email });
+    if( !validUser ) {
+      return next(errorHandler(404, "Invalid credentials"))
+    }
+    const validPassword = await bcryptjs.compareSync( password, validUser.password );
+    if( !validPassword ) {
+      return next(errorHandler(408, "Username or Email does not match"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET );
+    const { password: hashPassword, ...rest } = validUser._doc;
+    const expireDate = Date.now() * 360000;
+    res.cookie('access_token', token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+
+  } catch( error ) {
+    next( error );
+  }
+}
